8fdcf0c87f569cde52a3b83168192c1786401a81
sreevani8 <sreevanidumpala@gmail.com>
2025-04-24 12:46:59 +0530

    forcefully build fail

--- src/test/java/com/mulit/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/mulit/jenkin/api/SpringJenkinsApplicationTests.java
@@ -17,7 +17,7 @@ public class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 	@Test
-	public void contextLoads() {
+	public  contextLoads() {
 		logger.info("Test case executing...");
 		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);

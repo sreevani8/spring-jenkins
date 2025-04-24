3c6e1ec9626ae025f84501ddef55256be6e34d67
sreevani8 <sreevanidumpala@gmail.com>
2025-04-24 10:17:21 +0530

    second commit

--- src/test/java/com/mulit/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/mulit/jenkin/api/SpringJenkinsApplicationTests.java
@@ -2,7 +2,7 @@ package com.mulit.jenkin.api;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

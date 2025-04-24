a8966c1b54230ddc4cc23993dd9dfd2f3bec6130
sreevani8 <sreevanidumpala@gmail.com>
2025-04-24 10:14:02 +0530

    second commit

--- src/test/java/com/mulit/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/mulit/jenkin/api/SpringJenkinsApplicationTests.java
@@ -1,13 +1,25 @@
 package com.mulit.jenkin.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringJenkinsApplicationTests {
+public class SpringJenkinsApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		logger.info("Test case executing...");
+		logger.info("Test case executing second log statement...");
+		assertEquals(true, true);
 	}
 
 }

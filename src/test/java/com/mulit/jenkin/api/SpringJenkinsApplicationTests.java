3427c8c025812214322e39ca111bc2a5c830a1af
sreevani8 <sreevanidumpala@gmail.com>
2025-04-25 11:16:41 +0530

    removed logger statement

--- src/test/java/com/mulit/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/mulit/jenkin/api/SpringJenkinsApplicationTests.java
@@ -19,7 +19,6 @@ public class SpringJenkinsApplicationTests {
 	@Test
 	public  void contextLoads() {
 		logger.info("Test case executing...");
-		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);
 	}
 
